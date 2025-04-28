f0bcca5e0c2c7026103b3d5e4a86688c4cac1831
Oman0018 <oman0044@yahoo.co.uk>
2025-04-28 03:35:18 +0100

    Refactored MessageItem widget to improve image rendering and delete functionality

--- lib/widgets/message_item.dart
+++ lib/widgets/message_item.dart
@@ -1,18 +1,30 @@
 // lib/widgets/message_item.dart
+import 'dart:io';
 import 'package:flutter/material.dart';
+import '../models/message.dart'; // Make sure you have a Message model
 
 class MessageItem extends StatelessWidget {
-  final String content;
+  final Message message;
   final VoidCallback onDelete;
 
-  const MessageItem({super.key, required this.content, required this.onDelete});
+  const MessageItem({super.key, required this.message, required this.onDelete});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
       child: ListTile(
-        title: Text(content),
+        leading:
+            message.imagePath != null && message.imagePath!.isNotEmpty
+                ? Image.file(
+                  File(message.imagePath!),
+                  width: 50,
+                  height: 50,
+                  fit: BoxFit.cover,
+                )
+                : const Icon(Icons.image_not_supported),
+        title: Text(message.text),
+        subtitle: Text(message.timestamp.toString()),
         trailing: IconButton(
           icon: const Icon(Icons.delete, color: Colors.red),
           onPressed: onDelete,
